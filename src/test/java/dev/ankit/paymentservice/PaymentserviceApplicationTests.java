77a60973ca40486539b6a453307a8d67ea845e97
Ankit Arora <ankit@Ankits-MacBook-Pro-2.local>
2024-05-04 20:47:50 +0530

    First commit

---
+++ src/test/java/dev/ankit/paymentservice/PaymentserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ankit.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
